bafcc1636323b28ff9671e9daef35171543fd5a7
RaviKanth-31 <74865487+RaviKanth-31@users.noreply.github.com>
2023-07-09 20:54:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Matrix from './components/matrix';
+function App() {
+  return (
+    <div>
+      <h1 style={{textAlign:"center"}}>Sudoku Solver</h1>
+      <Matrix/>
+    </div>
+  );
+}
+
+export default App;
